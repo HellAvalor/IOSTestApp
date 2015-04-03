a23c3b8c1e6685d4295000b9aefa052a0faad5c4
Yuliya Karaman <yuka@ciklum.com>
2015-04-03 12:40:55 +0300

    Init commit

---
+++ DevComTest/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  DevComTest
+//
+//  Created by Yuliya Karaman on 4/3/15.
+//  Copyright (c) 2015 Andrew Karaman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var requestText: String = ""
+    var  md5: String = ""
+    
+    @IBOutlet weak var requestTextLabel: UILabel!
+    
+    @IBOutlet weak var md5Label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // md5Label.text = md5
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- DevComTest/ViewController.swift
+++ DevComTest/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    var requestText: String = ""
+    var md5: String = ""
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadingIndicator.hidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func requestButton(sender: AnyObject) {
+            println("test")
+        
+        loadingIndicator.hidden = false
+        let view2 = self.storyboard?.instantiateViewControllerWithIdentifier("view2") as SecondViewController
+        
+        self.navigationController?.pushViewController(view2, animated: false)
+
+        var url : String = "http://md5.jsontest.com/?text=" + inputField.text
+        
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        request.URL = NSURL(string: url)
+        request.HTTPMethod = "GET"
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler: {(response:NSURLResponse!, data: NSData!, error: NSError!)-> Void in
+        
+            var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+            let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
+            
+            if (jsonResult != nil)
+            {
+                println("test222")
+//                self.md5 = jsonResult.debugDescription
+               view2.md5Label.text = jsonResult.debugDescription
+            } else {
+            
+            }
+        
+        })
+        
+
+        
+    }
 
 }
 
