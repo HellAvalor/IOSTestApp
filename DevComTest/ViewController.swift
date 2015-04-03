50565ce02ebcfacd3f7432f41ed12ed0619ec221
Yuliya Karaman <yuka@ciklum.com>
2015-04-03 09:19:37 +0300

    Initial Commit

---
+++ DevComTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevComTest
+//
+//  Created by Yuliya Karaman on 4/3/15.
+//  Copyright (c) 2015 Andrew Karaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
